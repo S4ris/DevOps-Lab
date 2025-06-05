c4dec42932547ad79aef5e057ae750def59c6349
Adam Wiszniewski <wiszniewskiadam95@gmail.com>
2025-06-05 22:48:13 +0200

    Add folder with empty python script and provision file

---
+++ provision.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Update system
+sudo apt update
+
+#Instal python and pip
+sudo apt install -y python3 python3-pip
+
+#Create folder for app
+mkdir -p/home/vagrant/app
+
+#Copy serwer files into VM
+cp /vagrant/app/server.py /home/vagrant/app/
+
+#Run the application
+nohup python3 /home/vagrant/app/server.py
